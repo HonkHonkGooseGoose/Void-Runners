99603cb189aaddd5282cc82460aa291e42632c78
HonkHonkGooseGoose <minecraft420noice@gmail.com>
2025-07-02 11:20:22 -0700

    Refactor obstacle management in MainGame: streamline obstacle loading and improve length tracking

--- Assets/Scripts/MainGame.cs
+++ Assets/Scripts/MainGame.cs
@@ -1,139 +1,128 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
-using UnityEditor.Rendering;
-using NUnit.Framework;
+using UnityEngine.SceneManagement;
 
 public class MainGame : MonoBehaviour
 {
     System.Random rnd = new System.Random();
     int maxLoadedObstacles;
-
+    public List<float>  lengths         = new List<float>();
+    public List<string> loadedObstacles = new List<string>();
 
     public List<string> obstacleList = new List<string>()
     {
-      "pranav1",
-      "pranav2",
-      "pranav3",
-      "pranav4",
-      "ryan1",
-      "ryan2",
-      "ryan3",
-      "ryan4",
-      "maria1",
-      "maria2",
-      "maria3",
-
+        "pranav1","pranav2","pranav3","pranav4",
+        "ryan1","ryan2","ryan3","ryan4",
+        "maria1","maria2","maria3"
     };
 
-    public List<int> lengths;
+    // Track where the next obstacle should begin (world-space X)
+    float nextSpawnX = 0f;
 
-    public List<string> loadedObstacles;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         maxLoadedObstacles = 4;
 
-        // fill the loaded obstacles with the first 4 obstacles by using PickRandomScene and then adding them
         for (int j = 0; j < maxLoadedObstacles; j++)
         {
-            loadedObstacles.Add(PickRandomObstacle());
+            string newObstacle = PickRandomObstacle();
+            AddObstacle(newObstacle);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        // static int obsticalSelect(Array Obstical)
-        // {
+        float playerX = transform.position.x;              // script is on the player
+        int currentIdx = GetObjectIndex(playerX, lengths);
 
-        // }
-        Console.WriteLine("Update");
-
-    }
-
-    private void GenerateObstacles(int playerPosition)
-    {
-        int expectedObsticle;
-        int expectedPlayerPosition = 2;
-        int difference = playerPosition - expectedPlayerPosition;
-        System.Random select = new System.Random();
-        if (difference == 0)
+        if (currentIdx > 0)                                // player left first segment
         {
-            return;
-        }
-        else
-        {
-            for (int i = 0; i < difference; i++)
-            {
-                string newObstacle = PickRandomObstacle();
-                AddObstacle(newObstacle);
-                RemoveObstacle(loadedObstacles[0]);
-            }
-            
+            RemoveObstacle(loadedObstacles[0]);
+            string newObstacle = PickRandomObstacle();
+            AddObstacle(newObstacle);
         }
     }
 
-    private string PickRandomObstacle()
+    string PickRandomObstacle()
     {
-
-        string selectedObstacle = "";
+        string selected;
         do
         {
-            int i = rnd.Next(0, obstacleList.Count);
-            selectedObstacle = obstacleList[i];
+            selected = obstacleList[rnd.Next(obstacleList.Count)];
+        } while (loadedObstacles.Contains(selected));
 
-        } while (loadedObstacles.Contains(selectedObstacle));
-        // Pick a random scene from the obstacleList and return the name of the obstacle if it is not in loadedObstacles already
-
-        // while a object hasnt been picked
-
-        // pick a random string from obstacleList
-
-        // check if it isn't in loadedObstacles
-
-
-        return selectedObstacle;
-    }
-
-    private void AddObstacle(string obstacleName)
-    {
-        // Add the obstacle to the loadedObstacles list
-
-        // Add the obstacle to the scene
-
-        // get the length of the obstacle and add it to the lengths list
-        loadedObstacles.Add(PickRandomObstacle());
-
-
+        return selected;
     }
 
 
-    private void RemoveObstacle(string obstacleName)
+    void AddObstacle(string obstacleName)
     {
-        // Remove the obstacle from the loadedObstacles list
+        loadedObstacles.Add(obstacleName);
 
-        // Remove the obstacle from the scene
-
-        // remove the length of the obstacle from the lengths list
-        loadedObstacles.RemoveAt(0);
-
-    }
-
-
-    private static int GetObjectIndex(int playerPosition, List<int> lengths)
-    {
-        int index = 0;
-        int remaningDistance = playerPosition;
-        while (index < lengths.Count)
+        SceneManager.LoadSceneAsync(obstacleName, LoadSceneMode.Additive)
+                    .completed += (AsyncOperation op) =>
         {
-            remaningDistance -= lengths[index];
-            if (remaningDistance < 0)
-                return index;
-            index++;
+            Scene sc = SceneManager.GetSceneByName(obstacleName);
+
+            // Find the obstacle container inside the loaded scene
+            foreach (GameObject root in sc.GetRootGameObjects())
+            {
+                if (root.name != "ObstacleContainer") continue;
+
+                // Locate start/end anchors
+                Transform startTf = root.transform.Find("Start");
+                Transform endTf   = root.transform.Find("End");
+
+                if (startTf == null || endTf == null)
+                {
+                    Debug.LogError($"Start/End anchors missing in '{obstacleName}'");
+                    return;
+                }
+
+                float segLength = Vector2.Distance(startTf.position, endTf.position);
+
+                // Shift entire obstacle so that its Start meets nextSpawnX
+                float offset = nextSpawnX - startTf.position.x;
+                root.transform.position += new Vector3(offset, 0f, 0f);
+
+                // Update bookkeeping
+                lengths.Add(segLength);
+                nextSpawnX += segLength;
+                return;
+            }
+
+            Debug.LogError($"ObstacleContainer not found in scene '{obstacleName}'");
+        };
+    }
+
+    
+    void RemoveObstacle(string obstacleName)
+    {
+        SceneManager.UnloadSceneAsync(obstacleName);
+
+        loadedObstacles.RemoveAt(0);
+        float removedLen = lengths[0];
+        lengths.RemoveAt(0);
+
+        nextSpawnX -= removedLen;                           // track shrinks by that length
+    }
+
+    
+    static int GetObjectIndex(float playerPosX, List<float> segLengths)
+    {
+        int idx = 0;
+        float remaining = playerPosX;
+
+        while (idx < segLengths.Count)
+        {
+            remaining -= segLengths[idx];
+            if (remaining < 0f) return idx;
+            idx++;
         }
-        return index;
+        return idx;
     }
 }
