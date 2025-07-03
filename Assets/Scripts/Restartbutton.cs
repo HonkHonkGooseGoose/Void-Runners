f06a98034a0ff6b47b64304aff80a254e8a0ee86
mariastat9 <mstatnikova0@gmail.com>
2025-07-03 14:38:11 -0700

    Added Restart Button That goes to Main game

--- Assets/Scripts/Restartbutton.cs
+++ Assets/Scripts/Restartbutton.cs
@@ -1,22 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class Restartbutton : MonoBehaviour
+using UnityEngine.EventSystems;
+public class Restartbutton : MonoBehaviour, IPointerClickHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    //// Start is called once before the first execution of Update after the MonoBehaviour is created
+    //void Start()
+    //{
         
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            Debug.Log("CLICKED");
+            SceneManager.LoadScene("MainGame");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnMouseDown()
-    {
-        SceneManager.LoadScene("MainGame");
-    }
 }
